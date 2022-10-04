4a83a819394349bb46580cdec6c4832b88b6edb4
Anas Samoudi <unlimiteddez@gmail.com>
2022-10-04 06:13:15 +0100

    Fixed my cart page bug

--- client/components/ShoppingCart/ShoppingCartItem.tsx
+++ client/components/ShoppingCart/ShoppingCartItem.tsx
@@ -1,71 +1,103 @@
 import styled from "styled-components";
-import { AiOutlineClose } from "react-icons/ai";
-import InfoIMG from "../../assets/info1.jpg";
+import { AiOutlineDelete } from "react-icons/ai";
 import { useState } from "react";
+import { Product } from "../../types/product";
+import urlFor from "../../lib/ImageBuilder";
 
-const ShoppingCartItem = () => {
-  const [quantity, setQuantity] = useState<number>(1);
-  return (
-    <CartTable>
-      <AiOutlineClose className="cart-item" />
-      <img src={InfoIMG} alt="" className="product-thumbnail" />
-      <h3 className="product-name">Product Name</h3>
-      <p className="product-price">19.00$</p>
+interface CartItemsProps {
+    product: Product;
+    quantity: number;
+}
 
-      <div className="quantity">
-        <p className="quantity-btn" onClick={() => setQuantity(quantity - 1)}>
-          -
-        </p>
-        <p>{quantity}</p>
-        <p className="quantity-btn" onClick={() => setQuantity(quantity + 1)}>
-          +
-        </p>
-      </div>
-      <p>19.00$</p>
-    </CartTable>
-  );
+const ShoppingCartItem: React.FC<CartItemsProps> = ({ product, quantity }) => {
+    const [itemQuantity, setItemQuantity] = useState<number>(quantity);
+    return (
+        <CartTable>
+            <AiOutlineDelete className="cart-delete-icon" />
+            <img
+                src={urlFor(product.image[0]).url()}
+                alt=""
+                className="product-thumbnail"
+            />
+            <h3 className="product-name">{product.name}</h3>
+            <p className="product-price">{product.price}$</p>
+
+            <div className="quantity">
+                <p
+                    className="quantity-btn"
+                    onClick={() => setItemQuantity(itemQuantity - 1)}
+                >
+                    -
+                </p>
+                <p>{itemQuantity}</p>
+                <p
+                    className="quantity-btn"
+                    onClick={() => setItemQuantity(itemQuantity + 1)}
+                >
+                    +
+                </p>
+            </div>
+            <p>{product.price * itemQuantity}$</p>
+        </CartTable>
+    );
 };
 
 export default ShoppingCartItem;
 
 const CartTable = styled.div`
-  margin-block: 2rem;
-  flex-grow: 1;
-  flex-shrink: 1;
-  flex-basis: 60%;
-  display: grid;
-  grid-template-columns: 3rem 6rem 3fr 1fr 1fr 1fr;
-  align-items: center;
-  gap: 1.5rem;
-
-  .product-thumbnail {
-    width: 100%;
-    height: 6rem;
-    object-fit: cover;
-  }
-
-  .quantity {
-    display: flex;
+    margin-block: 2rem;
+    flex-grow: 1;
+    flex-shrink: 1;
+    flex-basis: 60%;
+    display: grid;
+    grid-template-columns: 3rem 6rem 3fr 1fr 1fr 1fr;
     align-items: center;
-    gap: 0.25rem;
-    color: #574545;
+    gap: 1.5rem;
 
-    & > p {
-      width: 2rem;
-      height: 2rem;
-      display: flex;
-      justify-content: center;
-      align-items: center;
+    .product-thumbnail {
+        width: 100%;
+        height: 6rem;
+        object-fit: cover;
     }
 
-    .quantity-btn {
-      font-weight: 800;
-      font-size: 1.5rem;
-      &:hover {
-        cursor: pointer;
-        background-color: #574545;
-        color: white;
-      }
+    .cart-delete-icon {
+        width: 2rem;
+        height: 2rem;
+        padding: 0.375rem;
+        border-radius: 50%;
+        fill: #ff1000;
+        background-color: #ff100010;
+        border: 1px solid #ff1000;
+
+        &:hover {
+            cursor: pointer;
+            fill: white;
+            background-color: #ff1000;
+        }
     }
-  }
+
+    .quantity {
+        display: flex;
+        align-items: center;
+        gap: 0.25rem;
+        color: #574545;
+
+        & > p {
+            width: 2rem;
+            height: 2rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .quantity-btn {
+            font-weight: 800;
+            font-size: 1.5rem;
+            &:hover {
+                cursor: pointer;
+                background-color: #574545;
+                color: white;
+            }
+        }
+    }
 `;
--- client/pages/my-cart.tsx
+++ client/pages/my-cart.tsx
@@ -1,136 +1,144 @@
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 import ShoppingCartItem from "../components/ShoppingCart/ShoppingCartItem";
+import { RootState } from "../store";
 
 const ShoppingCart = () => {
-  return (
-    <>
-      <Title>Shopping Cart</Title>
-      <CartDataWrapper className="page-container">
-        <div>
-          <CartTable>
-            <p></p>
-            <h2>Product</h2>
-            <p></p>
-            <h2>Price</h2>
-            <h2>Quantity</h2>
-            <h2>Subtotal</h2>
-          </CartTable>
-          <ShoppingCartItem />
-          <ShoppingCartItem />
-          <ShoppingCartItem />
-          <ShoppingCartItem />
-        </div>
-        <OverAllData>
-          <div className="overall-data">
-            <h2>Cart totals</h2>
-            <div className="flex">
-              <p className="title">Subtotal</p>
-              <p className="price">19.00 $</p>
-            </div>
-            <div className="flex">
-              <p className="title">Shipping</p>
-              <p className="price">0 $</p>
-            </div>
-            <div className="flex">
-              <p className="title">Total</p>
-              <p className="price">19.00 $</p>
-            </div>
-            <button className="checkout">PROCEED TO CHECKOUT</button>
-          </div>
-        </OverAllData>
-      </CartDataWrapper>
-    </>
-  );
+    const cart = useSelector((state: RootState) => state.cart);
+    return (
+        <>
+            <Title>Shopping Cart</Title>
+            <CartDataWrapper className="page-container">
+                <div>
+                    <CartTable>
+                        <p></p>
+                        <h2>Product</h2>
+                        <p></p>
+                        <h2>Price</h2>
+                        <h2>Quantity</h2>
+                        <h2>Subtotal</h2>
+                    </CartTable>
+                    {cart.map((cartItem) => (
+                        <ShoppingCartItem
+                            key={cartItem.product._id}
+                            product={cartItem.product}
+                            quantity={cartItem.quantity}
+                        />
+                    ))}
+                </div>
+                <OverAllData>
+                    <div className="overall-data">
+                        <h2>Cart totals</h2>
+                        <div className="flex">
+                            <p className="title">Subtotal</p>
+                            <p className="price">19.00 $</p>
+                        </div>
+                        <div className="flex">
+                            <p className="title">Shipping</p>
+                            <p className="price">0 $</p>
+                        </div>
+                        <div className="flex">
+                            <p className="title">Total</p>
+                            <p className="price">19.00 $</p>
+                        </div>
+                        <button className="checkout">
+                            PROCEED TO CHECKOUT
+                        </button>
+                    </div>
+                </OverAllData>
+            </CartDataWrapper>
+        </>
+    );
 };
 
 export default ShoppingCart;
 
 const Title = styled.h1`
-  text-align: center;
-  font-family: "PT Serif", serif;
-  margin-block: 8rem;
+    text-align: center;
+    font-family: "PT Serif", serif;
+    margin-block: 8rem;
 `;
 
 const CartDataWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 2fr 1fr;
-  gap: 4rem;
-  margin-block: 5rem 20rem;
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    gap: 4rem;
+    margin-block: 5rem 20rem;
 `;
 
 const CartTable = styled.div`
-  flex: 1 1 60%;
-  display: grid;
-  grid-template-columns: 3rem 6rem 3fr 1fr 1fr 1fr;
-  align-items: center;
-  gap: 1.5rem;
-
-  .product-thumbnail {
-    width: 100%;
-    height: 6rem;
-    object-fit: cover;
-  }
-
-  .quantity {
-    display: flex;
+    flex: 1 1 60%;
+    display: grid;
+    grid-template-columns: 3rem 6rem 3fr 1fr 1fr 1fr;
     align-items: center;
-    gap: 0.25rem;
-    color: #574545;
+    gap: 1.5rem;
 
-    & > p {
-      width: 2rem;
-      height: 2rem;
-      display: flex;
-      justify-content: center;
-      align-items: center;
+    .product-thumbnail {
+        width: 100%;
+        height: 6rem;
+        object-fit: cover;
     }
 
-    .quantity-btn {
-      font-weight: 800;
-      font-size: 1.5rem;
-      &:hover {
-        cursor: pointer;
-        background-color: #574545;
-        color: white;
-      }
+    .quantity {
+        display: flex;
+        align-items: center;
+        gap: 0.25rem;
+        color: #574545;
+
+        & > p {
+            width: 2rem;
+            height: 2rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .quantity-btn {
+            font-weight: 800;
+            font-size: 1.5rem;
+            &:hover {
+                cursor: pointer;
+                background-color: #574545;
+                color: white;
+            }
+        }
     }
-  }
 `;
 
 const OverAllData = styled.div`
-  font-size: 1.5rem;
+    font-size: 1.5rem;
 
-  .overall-data {
-    background-color: #574545;
-    padding: 2rem;
-    color: white;
-  }
-
-  .flex {
-    margin-top: 2rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    font-size: 1rem;
-
-    .title {
-      font-size: 1.1rem;
+    .overall-data {
+        background-color: #574545;
+        padding: 2rem;
+        color: white;
     }
 
-    .price {
-      color: #eee;
-      font-weight: 300;
-    }
-  }
+    .flex {
+        margin-top: 2rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        font-size: 1rem;
 
-  .checkout {
-    font-family: "Montserrat", sans-serif;
-    background-color: white;
-    color: #574545;
-    font-weight: 600;
-    width: 100%;
-    padding-block: 1rem;
-    border: transparent;
-    margin-top: 4rem;
-  }
+        .title {
+            font-size: 1.1rem;
+        }
+
+        .price {
+            color: #eee;
+            font-weight: 300;
+        }
+    }
+
+    .checkout {
+        font-family: "Montserrat", sans-serif;
+        background-color: white;
+        color: #574545;
+        font-weight: 600;
+        width: 100%;
+        padding-block: 1rem;
+        border: transparent;
+        margin-top: 4rem;
+    }
 `;
