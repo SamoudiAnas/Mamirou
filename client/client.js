19f713f0be965abada5f50aa061e85868272df1d
Anas Samoudi <unlimiteddez@gmail.com>
2022-10-07 13:44:16 +0100

    added sanity token

--- client/client.js
+++ client/client.js
@@ -4,4 +4,7 @@ export default sanityClient({
     projectId: "60c0gyn4",
     dataset: "production",
     useCdn: true,
+    apiVersion: "2022-03-10",
+    useCdn: true,
+    token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 });
