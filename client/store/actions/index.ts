77d5f4fadde8fe24dd2bf21b82e21185dc5ee89c
Anas Samoudi <unlimiteddez@gmail.com>
2022-11-21 02:41:26 +0100

    fixed import errors

--- client/store/actions/index.ts
+++ client/store/actions/index.ts
@@ -2,4 +2,3 @@ export * as Action from "./filters-action";
 export * as CartUIAction from "./cartUi-actions";
 export type { CartAction } from "./cart";
 export type { WishlistAction } from "./wishlist";
-export type { Toast as ToastAction } from "./toast";
